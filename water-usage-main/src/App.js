e2c56b732a9ac19112a04af113cc13eda0de7e66
LaffSteven <steve.laff@gmail.com>
2022-11-17 18:30:02 -0500

    added image

--- water-usage-main/src/App.js
+++ water-usage-main/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap';
 
 //import for bootstrap css
 import NavbarComponent from './components/Navbar';
--- water-usage-main/src/components/Hero.js
+++ water-usage-main/src/components/Hero.js
@@ -6,8 +6,8 @@ const Hero = () => {
 	return (
 		<>
 			<div class="container">
-				
-				<div class="d-flex flex-row shadow p-3 mb-5 bg-white rounded">
+				<img src="https://publicinterestnetwork.org/wp-content/uploads/2021/03/waste_is_out_of_fashion_shutterstock-1500x844.jpg" class=""></img>
+				<div class="d-flex flex-row shadow p-3 mb-5 bg-white rounded sticky-top">
 					<h1>Earthly Made</h1>
 				</div>
 				<div class="d-flex flex-row alignitems-left">
@@ -15,11 +15,9 @@ const Hero = () => {
 						<h3 class="text-left">Transforming the was we engage with fashion...</h3>
 						<h3 class="text-left">Find and learn sustainable fashion...</h3>
 					</div>
-
 				</div>
 			</div>
 		</>
-			
 	)
 
 }
