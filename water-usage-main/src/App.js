705294a03def99d6cd99e55c81239b81b3d7023d
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-11-17 09:51:17 -0800

    fixed comments

--- water-usage-main/src/App.js
+++ water-usage-main/src/App.js
@@ -9,15 +9,17 @@ function App() {
   return (
     <div className="App">
       <header>
+        {/* css for bootstrap */}
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
           integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
           crossorigin="anonymous"
         />
+        {/* js for bootstrap */}
         <script
-  src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
-  crossorigin></script>
+        src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+        crossorigin></script>
       </header>
       <Navbar>
         <h1>test</h1>
