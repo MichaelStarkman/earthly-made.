bc137d8ece4876103b98cb94ffd2f69e24cc145d
LaffSteven <steve.laff@gmail.com>
2022-11-17 18:14:20 -0500

    basic text added

--- water-usage-main/src/components/Hero.js
+++ water-usage-main/src/components/Hero.js
@@ -6,8 +6,16 @@ const Hero = () => {
 	return (
 		<>
 			<div class="container">
-				<div class="d-flex flex-row">
+				
+				<div class="d-flex flex-row shadow p-3 mb-5 bg-white rounded">
 					<h1>Earthly Made</h1>
+				</div>
+				<div class="d-flex flex-row alignitems-left">
+					<div class="d-flex flex-column justify-content-start shadow p-3 mb-5 bg-white rounded">
+						<h3 class="text-left">Transforming the was we engage with fashion...</h3>
+						<h3 class="text-left">Find and learn sustainable fashion...</h3>
+					</div>
+
 				</div>
 			</div>
 		</>
