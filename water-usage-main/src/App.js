5f3f63c49ec2cc3749313275f059b3c370483593
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-11-18 14:31:59 -0800

    fixed issues

--- water-usage-main/src/App.js
+++ water-usage-main/src/App.js
@@ -1,25 +1,17 @@
-// import Map from './components/Map'
 import './App.css';
 import 'bootstrap';
 
 //import for bootstrap css
+import Map from './components/Map'
 import Carousel from './components/Carousel'
-import ModalComponent from './components/modal'
 import Hero from './components/Hero.jsx';
-import Cards from './components/Cards'
-import Articles from './components/Articles';
 
 
 function App() {
   return (
     <div className="App">
-      <NavbarComponent />
-      {/* <Hero /> */}
+      <Hero />
       <Carousel />
-      {/* <Cards />
-      <Hero /> {/* .Cards now lives in here */}
-      <Carousel />
-      {/* <Cards />  Moved these Cards into the Hero section     */}
       <Map />
     </div>
   );
