dabf7547baeaec7ea47e9056e9efa5cce6b27b12
LaffSteven <steve.laff@gmail.com>
2022-11-18 10:21:39 -0500

    hero section formatting

--- water-usage-main/src/components/Hero.js
+++ water-usage-main/src/components/Hero.js
@@ -4,14 +4,15 @@ import 'bootstrap'
 const Hero = () => {
 
 	return (
-		<>
+		<>			
 			<div class="container-fluid"> 
 				<div class="rpw justify-content-center"> 
 					<img src="https://nypost.com/wp-content/uploads/sites/2/2021/11/Chile-clothing_03.jpg?quality=90&strip=allhttps://i.guim.co.uk/img/media/b96fba1cf27f712298e3e6c86bd14cd47fd0083a/531_4_3173_1904/master/3173.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=c1900b0faa14f5886d14e7fe1b9935ab" alt="test" class="img-responsive" /> 
-					<div class="carousel-caption d-flex flex-column bg-primary "> 
+					<div class="carousel-caption d-flex flex-column text-left align-items-start"> 
 						<h1>Earthly Made</h1> 
-						<h2>Transforming the way we engage with fashion</h2>
-						<h2>Find and learn sustainable fashion</h2>
+						<br />
+						<h3>Transforming the way we engage with fashion</h3>
+						<h3>Find and learn sustainable fashion</h3>
 					</div> 
 				</div> 
 			</div> 
@@ -21,9 +22,3 @@ const Hero = () => {
 }
 
 export default Hero;
-
-{/* <div>
-	<h1>Earthly</h1>
-	<h1>Made</h1>
-</div> */}
-
