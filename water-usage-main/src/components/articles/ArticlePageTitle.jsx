14839f11424bcaedbf3eff2c6e5d14899a8db0e5
LaffSteven <steve.laff@gmail.com>
2022-11-18 12:46:34 -0500

    updated ArticleThumbnail

--- water-usage-main/src/components/articles/ArticlePageTitle.jsx
+++ water-usage-main/src/components/articles/ArticlePageTitle.jsx
@@ -1,5 +1,17 @@
 const ArticlePageTitle = () => {
-
+	return (
+		<>			
+			<div class="container-fluid"> 
+				<div class="rpw justify-content-center"> 
+					<div class="d-flex flex-column text-left align-items-start"> 
+						<h1 class="display-3">Articles</h1> 
+						<br />
+						<h2>Learn Why Sustainability Matters</h2>
+					</div> 
+				</div> 
+			</div> 
+		</>
+	)
 }
 
 export default ArticlePageTitle
