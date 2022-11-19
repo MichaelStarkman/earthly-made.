1c2961d09c3dc3df4be31ef9ce86110c93234a69
LaffSteven <steve.laff@gmail.com>
2022-11-19 12:14:13 -0500

    roughing in small article

--- water-usage-main/src/components/articles/ArticleCard.jsx
+++ water-usage-main/src/components/articles/ArticleCard.jsx
@@ -1,9 +1,16 @@
-const ArticleCard = () => {
+const ArticleCard = (props) => {
 	return (
-		<div class="card-img-top">
-			<img src="" alt="" />
-			<div>
-				<p></p>
+		<div class="article-wrapper-sm">
+			<div class="article-img-sm">
+				<img src={props.article.image} alt="" />
+			</div>
+			<div class="article-caption-sm d-flex flex-row justify-content-between">
+				<p>{props.article.date}</p>
+				<p>{props.article.author}</p>
+			</div>
+			<div class="article-text-sm">
+				<h4>{props.article.title}</h4>
+				<p>{props.article.summary}</p>
 			</div>
 		</div>
 	)
--- water-usage-main/src/components/articles/FeaturedArticles.jsx
+++ water-usage-main/src/components/articles/FeaturedArticles.jsx
@@ -1,5 +1,15 @@
 import ArticleCard from "./ArticleCard"
 
+const testArticle = {
+	url: 'https://www.thegoodtrade.com/features/fair-trade-clothing',
+	image: 'https://images.squarespace-cdn.com/content/v1/61dcd32b3fb8bb4b5af9b560/1659998355568-GRO1WNHVIJT7EYS5CKCG/ethical-sustainable-clothing.jpeg',
+	altText: '',
+	title: '35 Sustainable Clothing Brands Betting Against Fast Fashion',
+	summary: 'What is the most environmentally friendly fashion brand? Many of these ethical clothing brands come with third-party sustainable and ethical certifications as well.',
+	date: "11/3/2022",
+	author: "the good trade editors"
+}
+
 const FeaturedArticles = () => {
 	return (
 		<section>
@@ -9,9 +19,9 @@ const FeaturedArticles = () => {
 						{/* Holds one article */}
 						Main Article
 					</div>
-					<div class="">
+					<div class="d-flex flex-column">
 						{/* Column with two smaller article cards */}
-						smaller articles
+						<ArticleCard article={testArticle}/>
 
 					</div>
 				</div>
