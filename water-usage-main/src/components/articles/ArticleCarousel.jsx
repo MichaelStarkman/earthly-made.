43a4fba4cd2e8909541cd20e2513a9b79e009d39
LaffSteven <steve.laff@gmail.com>
2022-11-20 09:51:24 -0500

    added comments

--- water-usage-main/src/components/articles/ArticleCarousel.jsx
+++ water-usage-main/src/components/articles/ArticleCarousel.jsx
@@ -1,3 +1,5 @@
+//	Will programatically add all articles in ArticleIndex array as slide to the carousel
+
 const ArticleCarousel = () => {
 	return (
 		<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
