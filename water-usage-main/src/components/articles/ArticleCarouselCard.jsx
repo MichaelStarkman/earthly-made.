6b849046fac0cdd2240fac89586fa4b914f94f3a
LaffSteven <steve.laff@gmail.com>
2022-11-20 18:52:55 -0500

    updated carousel

--- water-usage-main/src/components/articles/ArticleCarouselCard.jsx
+++ water-usage-main/src/components/articles/ArticleCarouselCard.jsx
@@ -4,7 +4,7 @@ import { Card, Button } from "react-bootstrap";
 const ArticleCarouselCard = (props) => {
 	return (
 		<div class="col">
-			<Card class="rounded-0" style={{height: 500 }}>
+			<Card class="border border-0 rounded-0" style={{height: 500 }}>
 				<Card.Img variant="top" src="https://images.unsplash.com/photo-1473861646675-0252edc45daa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHN1c3RhaW5hYmxlJTIwZmFzaGlvbnxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60"/>
 				<Card.Body>
 					<Card.Title>Article Title Goes Here</Card.Title>
