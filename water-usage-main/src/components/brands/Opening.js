fdf2f89af9f7b24247e469711b72e45a03b8df9d
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-11-21 12:24:45 -0800

    added different scaling

--- water-usage-main/src/components/brands/Opening.js
+++ water-usage-main/src/components/brands/Opening.js
@@ -11,9 +11,12 @@ const Opening = () => {
           <span className='oh1'>Sustainable</span>
           <span className='oh2'>Brands</span>
         </div>
-        <h3 className='oBlurb'>
-          Who makes the clothes we wear every day and in what kind of conditions? Our spending—and non-spending—can help make a positive difference for the people and resources involved in the making of our clothing. Each fashion brand has made it a central part of its mission to approach garment production in an ethical and transparent way that considers both people and the planet. Many of these ethical clothing brands come with third-party sustainable and ethical certifications as well.
-        </h3>
+        <div className='blurbContainer'>
+          <h3 className='oBlurb'>
+            Who makes the clothes we wear every day and in what kind of conditions? Our spending—and non-spending—can help make a positive difference for the people and resources involved in the making of our clothing. Each fashion brand has made it a central part of its mission to approach garment production in an ethical and transparent way that considers both people and the planet. Many of these ethical clothing brands come with third-party sustainable and ethical certifications as well.
+          </h3>
+          <span className='carrot'>⌄</span>
+        </div>
         <img className='openImg openImg1' src={farol} alt='farol standing' />
         
         <img className='openImg2 openImg' src={secondFarol} alt='farol standing' />
